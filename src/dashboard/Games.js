f9437b148beb4e1f8c390a1904c4c14baa431b27
Yanjin Chen <yanjinscience@gmail.com>
2021-06-28 09:42:30 +0800

    slider feature added, max/min feature to be added

--- src/dashboard/Games.js
+++ src/dashboard/Games.js
@@ -2,6 +2,13 @@ import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import axios from 'axios';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import RangeSlider from './RangeSlider';
 
 const columns = [
   { field: 'home', headerName: 'Home', width: 130 },
@@ -46,27 +53,90 @@ const columns = [
     width: 200,
   }
 ];
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 
 export default function Games() {
   const [rowData, setRowData] = React.useState([]);
+  const [rawData, setRawData] = React.useState([]);
+  const [max, setMax] = React.useState({home:"",away:"",spread:0,value:0,neutral:0,difference:0,pick:"",level:"",time:""});
+  const [min, setMin] = React.useState();
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleMove = (value,value1,value2) =>{
+    console.log(value,value1,value2);
+      const newData = rawData.filter(d => d.spread <= value[1] && d.spread >= value[0] && 
+        d.value <= value1[1] && d.value >=value1[0] && d.neutral <= value2[1] && d.neutral >= value2[0]) ;
+      setRowData(newData);
+      
+      //const newData2 = rowData.filter(d => d.value <= value1[1] && d.value >=value1[0]);
+      //setRowData(newData2);
+      //const newData3 = rowData.filter(d => d.neutral <= value2[1] && d.neutral >= value2[0]);
+      //setRowData(newData3);
+  
+  }
+  
+
+  const classes = useStyles();
+
   React.useEffect(() => {
-    var apiurl = "http://3.84.121.75:8080/schedule/GamesByDate/2021-02-26";
+    var apiurl = "http://3.84.121.75:8080/schedule/Games";
     axios
       .get(apiurl)
       .then((response) => response.data)
       .then((data) => {
         setIsLoaded(true);
-        setRowData(data.data);
-        console.log("rows" + rowData);
+        //setRowData(data.data);
+        setRawData(data.data);
+        const newdata = data.data;
+        setRowData(newdata);
       });
+        //const maxdata = rawData.sort(function(a, b){return a.spread - b.spread});
+        //console.log(typeof(maxdata));
+        //setMin(maxdata[0]);
+        //setMinmin(min.spread);
+        const mindata = rawData.sort(function(a, b){return b.spread - a.spread});
+        setMax(mindata[0]);
+        //setMaxmax(max.spread);
+        console.log(mindata[0]);
+        console.log(max.spread);
   }, []);
+  
+  
+  
+  
+  
+
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div>
+
+      <RangeSlider handleMove = {handleMove}/>
+
       {isLoaded ? <DataGrid rows={rowData} columns={columns} getRowId ={(rowData) => rowData.home} autoHeight={true} /> :
       <CircularProgress />}
       
     </div>
   );
 }
+
+
---
+++ src/dashboard/RangeSlider.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function RangeSlider({handleMove}) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState([-11, 25]);
+  const [value1, setValue1] = React.useState([-8, 25]);
+  const [value2, setValue2] = React.useState([-12.5, 25]);
+      
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    handleMove(value,value1,value2);
+   
+  };
+  const handleChange1 = (event, newValue) => {
+    setValue1(newValue);
+    handleMove(value,value1,value2);
+   
+  };
+  const handleChange2 = (event, newValue) => {
+    setValue2(newValue);
+    handleMove(value,value1,value2);
+    
+  };
+  
+  
+
+  return (
+    <div className={classes.root}>
+      <Typography id="range-slider" gutterBottom>
+         spread range
+      </Typography>
+      <Slider
+        min = {-11}
+        max = {25}
+        step = {0.5}
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+      <Typography id="range-slider" gutterBottom>
+         value range
+      </Typography>
+      <Slider
+        min = {-8}
+        max = {22}
+        step = {0.5}
+        value={value1}
+        onChange={handleChange1}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+      <Typography id="range-slider" gutterBottom>
+         neutral range
+      </Typography>
+      <Slider
+        min = {-12.5}
+        max = {16.5}
+        step = {0.5}
+        value={value2}
+        onChange={handleChange2}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+    </div>
+    
+    
+  );
+}
