1e647354114306d46b846ec2ad2fd6a473e26a4b
yanjin chen <60858109+YanjinChen2023@users.noreply.github.com>
2021-08-24 09:08:02 +0800

    Update PieChart.js

--- src/dashboard/PieChart.js
+++ src/dashboard/PieChart.js
@@ -5,15 +5,16 @@ function PieChart(props){
     console.log(props);
     let a = props.correct;
     let b = props.incorrect;
+    let c = props.name;
     const data = {
-        labels:['Close','NotClose'],
+        labels:[c,'Not'+c],
         datasets:[
             {
             label: 'prediction result',
             data:[a,b],
             backgroundColor:[
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 205, 86, 1)'  
+                'rgba(112, 140, 255, 1)',
+                'rgba(210, 65, 149, 1)'  
             ]
         }
         ]
@@ -27,7 +28,7 @@ function PieChart(props){
         responsive: false
 
     }
-    return <Doughnut data = {data} width={400}
-	height={400} options = {options}/>
+    return <Doughnut data = {data} width={300}
+	height={300} options = {options}/>
 }
 export default PieChart
